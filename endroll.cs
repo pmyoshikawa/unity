aeaeb3088451ca38474a75fc9860280918cdb33f
ALMOND007 <42898055+ALMOND007@users.noreply.github.com>
2018-09-02 05:04:48 +0900

    Add files via upload

---
+++ endroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endroll : MonoBehaviour {
+
+ int speed = 1;
+ private GameObject canvas;
+
+void Start () {
+    // TextCanvasを呼び出す
+    canvas = GameObject.Find("TextA");
+}
+
+void Update () {
+
+    // 0.05ずつy軸マイナス方向にtextを動かしていく
+    transform.Translate(0,0.6f*speed,0);
+    // textのy座標が-55以下になったらcanvasを削除
+    if (transform.localPosition.y >= 2200) {
+        Destroy(canvas);
+        Application.LoadLevel("goal");
+
+    }
+}
+}
