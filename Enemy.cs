f2d622d90ea256de0709cefe10853f56c0acbdf6
ALMOND007 <42898055+ALMOND007@users.noreply.github.com>
2018-09-02 04:59:46 +0900

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public GameObject target;
+    UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = target.transform.position;
+	}
+}
