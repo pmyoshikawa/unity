571a0f6bb2981c801879a4730abc0eff4f8acc8d
ALMOND007 <42898055+ALMOND007@users.noreply.github.com>
2018-09-02 04:52:58 +0900

    Add files via upload

---
+++ ESCAPE.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ESCAPE : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+        
+	#if UNITY_EDITOR
+
+		UnityEditor.EditorApplication.isPlaying = false;
+
+	#elif UNITY_WEBPLAYER
+	
+	#else
+		Application.Quit();
+	#endif
+}
+        }
+    }
