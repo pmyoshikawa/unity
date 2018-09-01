c397c4bf6756b363d7159190da5b4b6aa91f2339
ALMOND007 <42898055+ALMOND007@users.noreply.github.com>
2018-09-02 04:47:55 +0900

    Add files via upload

---
+++ indy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class indy : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+
+		if (collision.gameObject.name == "PlayerObj")
+		{
+
+			Destroy (gameObject);
+
+
+
+		}
+
+	}
+
+}
